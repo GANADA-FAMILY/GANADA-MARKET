9e933a934857d3561be15ba1f651323c3ab84095
jio-kim <now20412041@gmail.com>
2022-04-19 13:37:30 +0900

    #S06P31D204 feat: Add router and code spliting

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Loading from "./components/Loading";
+
+const HomePage = React.lazy(() => import("./pages/HomePage"));
 
 const App = () => {
-  return <div>APP</div>;
+  return (
+    <Router>
+      <React.Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </React.Suspense>
+    </Router>
+  );
 };
 
 export default App;
