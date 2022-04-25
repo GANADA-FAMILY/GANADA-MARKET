23853f6eba2f23e25c350a8f6fdacef72f010732
이홍준 <zunza96@naver.com>
2022-04-26 02:53:53 +0900

    #S06P31D204-34 chore : 탭 칸막이 생성

--- FE/src/components/molecules/my/Tabs.tsx
+++ FE/src/components/molecules/my/Tabs.tsx
@@ -23,5 +23,14 @@ const Molecule = styled.div`
     width: 100%;
     background-color: #fafafa;
     border-radius: 12px;
+    div:first-of-type a:before{
+        content: "";
+        position: absolute;
+        top: 18px;
+        right: 0;
+        width: 1px;
+        bottom: 18px;
+        background-color: #ebebeb;
+    }
 `
 export default Tabs;
