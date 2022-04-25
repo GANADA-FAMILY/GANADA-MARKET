2f17b669c19b938a190ce499c44675737e0e578c
jio-kim <now20412041@gmail.com>
2022-04-25 15:18:07 +0900

    feat: temp merge

--- FE/src/index.tsx
+++ FE/src/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './state/Store';
 import './index.css';
-import App from './App';
+import Loading from './components/Loading';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
-root.render(
+const App = lazy(() => import('./App'));
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Suspense fallback={<Loading />}>
+        <App />
+      </Suspense>
+    </Provider>
   </React.StrictMode>,
 );
---
+++ FE/src/state/Store.ts
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+const Store = configureStore({
+  reducer: {},
+});
+
+export type RootState = ReturnType<typeof Store.getState>;
+export default Store;
