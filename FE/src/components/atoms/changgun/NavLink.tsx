411057314941d4afa20046036ea6eaa9c69d7e35
changgun <pcg0527@naver.com>
2022-05-09 16:45:36 +0900

    refactor : NavLink컴포넌트 css 헬퍼로 단순화

--- FE/src/components/atoms/changgun/NavLink.tsx
+++ FE/src/components/atoms/changgun/NavLink.tsx
@@ -1,30 +1,26 @@
-import React from 'react';
 import styled from 'styled-components';
-import { Link as DefaultLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-interface LinkProps {
-  to: string;
-  className?: string;
-  children: string;
+interface StyledLinkProps {
+  $isActive: boolean;
 }
 
-Link.defaultProps = {
-  className: '',
+const activeStyle = ({ $isActive }: StyledLinkProps) => {
+  if ($isActive) {
+    return `
+      font-weight: 700;
+      text-decoration: underline`;
+  }
+  return `
+    font-weight: 400;
+    text-decoration: none;
+  `;
 };
 
-function Link({ to, className, children }: LinkProps) {
-  return (
-    <DefaultLink className={className} to={to}>
-      {children}
-    </DefaultLink>
-  );
-}
-
-const StyledLink = styled(Link)<{ isActive: boolean }>`
+const StyledLink = styled(Link)<StyledLinkProps>`
   color: black;
   font-size: 1.5rem;
-  font-weight: ${(props) => (props.isActive ? '700' : '400')};
-  text-decoration: ${(props) => (props.isActive ? 'underline' : 'none')};
+  ${activeStyle}
 `;
 
 export default StyledLink;
--- FE/src/pages/TestPage.tsx
+++ FE/src/pages/TestPage.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
 import StyledLink from '../components/atoms/changgun/NavLink';
 
 function TestPage() {
   return (
     <>
-      <StyledLink to="/" isActive>
+      <StyledLink to="/" $isActive>
         hi
       </StyledLink>
-      <StyledLink to="/my" isActive>
+      <StyledLink to="/my" $isActive>
         hi
       </StyledLink>
     </>
