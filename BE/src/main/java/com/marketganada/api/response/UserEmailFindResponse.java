1c32c54792d78579188cc560fb5f2dc323b5981d
wnsdl2494 <wnsdl2494@gmail.com>
2022-05-13 14:28:43 +0900

    #S06P31D204-157 fix : Auth API response 수정

--- BE/src/main/java/com/marketganada/api/response/UserEmailFindResponse.java
+++ BE/src/main/java/com/marketganada/api/response/UserEmailFindResponse.java
@@ -1,37 +1,44 @@
 package com.marketganada.api.response;
 
-import com.marketganada.common.AES256;
+
 import com.marketganada.db.entity.User;
 import io.swagger.annotations.ApiModel;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @Setter
 @ApiModel("UserEmailFindResponse")
 public class UserEmailFindResponse extends BaseResponseBody{
-    List<String> emailList;
+    Map<String,Object> data;
+//    List<String> emailList;
 
     public static UserEmailFindResponse of(Integer statusCode, String message, List<User> userList) {
-
         UserEmailFindResponse res = new UserEmailFindResponse();
+        Map<String,Object> data = new HashMap<>();
+        List<Map<String,Object>> emailList = new ArrayList<>();
+
         res.setStatusCode(statusCode);
         res.setMessage(message);
 
-        List<String> emailList = new ArrayList<>();
+
         for(User user : userList){
+            Map<String, Object> emailInfo = new HashMap<>();
             String splitEmail[] = user.getUserEmail().split("@");
             StringBuffer sb = new StringBuffer(splitEmail[0]);
             for(int i=1; i<splitEmail[0].length()-1; i++){
                 sb.replace(i,i+1,"*");
             }
-            emailList.add(sb+"@"+splitEmail[1]);
+            emailInfo.put("userEmail",sb+"@"+splitEmail[1]);
+            emailList.add(emailInfo);
         }
-
-        res.setEmailList(emailList);
+        data.put("emailList",emailList);
+        res.setData(data);
 
         return res;
     }
