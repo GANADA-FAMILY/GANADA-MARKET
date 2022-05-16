f2d54bd2624a263d0436dd7274ae7a4bf51113dc
이홍준 <zunza96@naver.com>
2022-05-16 13:55:21 +0900

    chore : resposebody 제거

--- FE/src/types/ResponseBody.ts
+++
@@ -1,8 +0,0 @@
-import { AxiosInstance, AxiosResponse } from 'axios';
-import Entity from './Entity';
-
-export default interface ResponseBody extends AxiosInstance {
-  message: string;
-  statusCode: number;
-  data?: Entity | Array<Entity>;
-}
