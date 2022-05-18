aec3e819ccb811c6325149e70c389f1e99cbf0cc
wnsdl2494 <wnsdl2494@gmail.com>
2022-05-18 16:41:05 +0900

    fix : 테스트케이스 수정

--- BE/src/test/java/com/marketganada/controller/AuthControllerNegativeCases.java
+++ BE/src/test/java/com/marketganada/controller/AuthControllerNegativeCases.java
@@ -37,7 +37,7 @@ public class AuthControllerNegativeCases {
         input.put("userEmail", "tttttestcom");  // 유효성검사 Email 형식
         input.put("userPw", "test123!@#");
         input.put("userNickname", "테스트유저");
-        input.put("userPhone", "010-1234-1234");
+        input.put("userPhone", "01098989898");
         mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/signup")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(input)))
@@ -48,7 +48,7 @@ public class AuthControllerNegativeCases {
         input2.put("userEmail", "ttttt@test.com");
         input2.put("userPw", "t"); // 유효성검사 8~16자리
         input2.put("userNickname", "테스트유저");
-        input2.put("userPhone", "010-1234-1234");
+        input2.put("userPhone", "01098989898");
         mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/signup")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(input2)))
@@ -59,7 +59,7 @@ public class AuthControllerNegativeCases {
         input3.put("userEmail", "ttttt@test.com");
         input3.put("userPw", "test123!@#");
         input3.put("userNickname", "");  // 유효성검사 NotBlank
-        input3.put("userPhone", "010-1234-1234");
+        input3.put("userPhone", "01098989898");
         mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/signup")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(input3)))
@@ -78,25 +78,25 @@ public class AuthControllerNegativeCases {
                 .andDo(MockMvcResultHandlers.print());
 
         Map<String, String> input5 = new HashMap<>();
-        input5.put("userEmail", "test@ssafy.com"); // 중복 이메일
+        input5.put("userEmail", "ttttt@test.com"); // 중복 이메일
         input5.put("userPw", "test123!@#");
         input5.put("userNickname", "테스트유저2");
-        input5.put("userPhone", "010-1234-1234");
+        input5.put("userPhone", "01098989898");
         mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/signup")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(input5)))
-                .andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                .andExpect(MockMvcResultMatchers.status().isConflict())
                 .andDo(MockMvcResultHandlers.print());
 
         Map<String, String> input6 = new HashMap<>();
         input6.put("userEmail", "ttttt@test.com");
         input6.put("userPw", "test123!@#");
         input6.put("userNickname", "TestUser"); // 중복 닉네임
-        input6.put("userPhone", "010-1234-1234");
+        input6.put("userPhone", "01098989898");
         mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/signup")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(input6)))
-                .andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                .andExpect(MockMvcResultMatchers.status().isConflict())
                 .andDo(MockMvcResultHandlers.print());
     }
 
