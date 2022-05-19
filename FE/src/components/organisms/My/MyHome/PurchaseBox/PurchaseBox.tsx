a83bc670c54f7c441f94ec1784fb4a36375e5765
이홍준 <zunza96@naver.com>
2022-05-20 08:55:15 +0900

    chore: PurchaseBox 적용

--- FE/src/components/organisms/My/MyHome/PurchaseBox/PurchaseBox.tsx
+++ FE/src/components/organisms/My/MyHome/PurchaseBox/PurchaseBox.tsx
@@ -1,60 +1,104 @@
-import React from 'react';
 import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
 import { List, Tabs } from 'components/molecules/My';
 import { TabPane } from 'components/atoms/My';
+import { useRootDispatch, useRootSelector } from 'state/Hooks';
+import { useEffect } from 'react';
+import {
+  getFilteredOrderHistory,
+  getOrderHistory,
+  setTabIndex,
+} from 'state/reducers/OrderHistorySlice';
+import getSizeByStatus from 'functions/getSizeByStatus';
 
 function PurchaseBox() {
+  const dispatch = useRootDispatch();
+  const orderHistory = useRootSelector(
+    (state) => state.orderHistory.orderHistory,
+  );
+  useEffect(() => {
+    dispatch(getOrderHistory());
+  }, []);
   return (
     <StyledBox>
-      <Tabs>
-        <TabPane>
+      <StyledTab>
+        <TabPane
+          onClick={async () => {
+            await dispatch(setTabIndex(-1));
+            dispatch(getFilteredOrderHistory());
+          }}
+        >
           <TabLink to="/my/buying">
             <TabBox>
               <TabTitle>전체</TabTitle>
-              <TabCount>0</TabCount>
+              <TabCount>
+                {orderHistory.length === undefined ? 0 : orderHistory.length}
+              </TabCount>
             </TabBox>
           </TabLink>
         </TabPane>
-        <TabPane>
+        <TabPane
+          onClick={async () => {
+            await dispatch(setTabIndex(0));
+            dispatch(getFilteredOrderHistory());
+          }}
+        >
           <TabLink to="/my/buying">
             <TabBox>
               <TabTitle>입금 대기</TabTitle>
-              <TabCount>0</TabCount>
+              <TabCount>{getSizeByStatus(orderHistory, 0)}</TabCount>
             </TabBox>
           </TabLink>
         </TabPane>
-        <TabPane>
+        <TabPane
+          onClick={async () => {
+            await dispatch(setTabIndex(1));
+            dispatch(getFilteredOrderHistory());
+          }}
+        >
           <TabLink to="/my/buying">
             <TabBox>
               <TabTitle>입금 완료</TabTitle>
-              <TabCount>0</TabCount>
+              <TabCount>{getSizeByStatus(orderHistory, 1)}</TabCount>
             </TabBox>
           </TabLink>
         </TabPane>
-        <TabPane>
+        <TabPane
+          onClick={async () => {
+            await dispatch(setTabIndex(2));
+            dispatch(getFilteredOrderHistory());
+          }}
+        >
           <TabLink to="/my/buying">
             <TabBox>
               <TabTitle>배송 중</TabTitle>
-              <TabCount>0</TabCount>
+              <TabCount>{getSizeByStatus(orderHistory, 2)}</TabCount>
             </TabBox>
           </TabLink>
         </TabPane>
-        <TabPane>
+        <TabPane
+          onClick={async () => {
+            await dispatch(setTabIndex(3));
+            dispatch(getFilteredOrderHistory());
+          }}
+        >
           <TabLink to="/my/buying">
             <TabBox>
               <TabTitle>종료</TabTitle>
-              <TabCount>0</TabCount>
+              <TabCount>{getSizeByStatus(orderHistory, 3)}</TabCount>
             </TabBox>
           </TabLink>
         </TabPane>
-      </Tabs>
+      </StyledTab>
       <ListArea>
-        <List dataSoruce={[]} />
+        <List dataSoruce={orderHistory} />
       </ListArea>
     </StyledBox>
   );
 }
+const StyledTab = styled(Tabs)`
+  margin-bottom: 1.5rem;
+`;
 const TabLink = styled(Link)`
   position: relative;
   display: block;
