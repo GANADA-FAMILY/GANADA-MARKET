6d8da12c4cb396343b0e8afcd71d8fc502f765d0
김상희 <jkhb62012@gmail.com>
2022-05-19 14:34:09 +0900

    #S06P31D204-168 Feat: Auction List 조회 시 다중 필터 적용

--- BE/src/main/java/com/marketganada/common/ProductSpecification.java
+++ BE/src/main/java/com/marketganada/common/ProductSpecification.java
@@ -3,20 +3,80 @@ package com.marketganada.common;
 import com.marketganada.db.entity.Product;
 import org.springframework.data.jpa.domain.Specification;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductSpecification {
-    public static Specification<Product> equalProductName(String productName) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("productName"), productName);
+//    public static Specification<Product> equalProductName(String productName) {
+//        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("productName"), productName);
+//    }
+
+    public static Specification<Product> equalProductNames(List<String> productNames) {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = getPredicateOfProductName(productNames,root,criteriaBuilder);
+            return criteriaBuilder.or(predicates.toArray(new Predicate[0]));
+        };
     }
 
-    public static Specification<Product> equalProductBrand(String productBrand) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("productBrand"), productBrand);
+    private static List<Predicate> getPredicateOfProductName(List<String> names, Root<Product> root, CriteriaBuilder criteriaBuilder) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        for(String s : names) {
+            if(!s.equals("ALL"))
+                predicates.add(criteriaBuilder.equal(root.get("productName"), s));
+        }
+
+        return predicates;
+    }
+
+//    public static Specification<Product> equalProductBrand(String productBrand) {
+//        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("productBrand"), productBrand);
+//    }
+
+    public static Specification<Product> equalProductBrands(List<String> productBrands) {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = getPredicateOfProductBrand(productBrands,root,criteriaBuilder);
+            return criteriaBuilder.or(predicates.toArray(new Predicate[0]));
+        };
+    }
+
+    private static List<Predicate> getPredicateOfProductBrand(List<String> names, Root<Product> root, CriteriaBuilder criteriaBuilder) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        for(String s : names) {
+            if(!s.equals("ALL"))
+                predicates.add(criteriaBuilder.equal(root.get("productBrand"), s));
+        }
+
+        return predicates;
     }
 
     public static Specification<Product> equalCategoryLargeName(String categoryLargeName) {
         return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("categoryLarge").get("name"), categoryLargeName);
     }
 
-    public static Specification<Product> equalCategorySmallName(String categorySmallName) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("categorySmall").get("name"), categorySmallName);
+//    public static Specification<Product> equalCategorySmallName(String categorySmallName) {
+//        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("categorySmall").get("name"), categorySmallName);
+//    }
+
+    public static Specification<Product> equalCategorySmallNames(List<String> categorySmallNames) {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = getPredicateOfCategorySmallName(categorySmallNames,root,criteriaBuilder);
+            return criteriaBuilder.or(predicates.toArray(new Predicate[0]));
+        };
+    }
+
+    private static List<Predicate> getPredicateOfCategorySmallName(List<String> names, Root<Product> root, CriteriaBuilder criteriaBuilder) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        for(String s : names) {
+            if(!s.equals("ALL"))
+                predicates.add(criteriaBuilder.equal(root.get("categorySmall").get("name"), s));
+        }
+
+        return predicates;
     }
 }
