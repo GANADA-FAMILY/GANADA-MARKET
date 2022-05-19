f024aedd7fe2b7628f02534d9d065c184bc68d53
wnsdl2494 <wnsdl2494@gmail.com>
2022-05-20 05:09:21 +0900

    fix : 판매내역 구매내역 프로필 조회 response 수정, 현재가격 계산식 수정

--- BE/src/main/java/com/marketganada/api/service/AuctionServiceImpl.java
+++ BE/src/main/java/com/marketganada/api/service/AuctionServiceImpl.java
@@ -295,4 +295,10 @@ public class AuctionServiceImpl implements AuctionService {
 
         return auctionCnt;
     }
+
+    @Override
+    public List<Auction> getTrueAuctionListByUser(User user) {
+        List<Auction> selling = auctionRepository.findByUserAndAuctionStatus(user,true);
+        return selling;
+    }
 }
